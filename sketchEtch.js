169925bbb5810a3f62fee5efe46ea021b7702133
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-29 04:52:32 -0700

    Refactor into function, starting to see lines
    - Trying to figure out how I can do this with not very much code, but may have to make seperate functions for rows and columns

--- sketchEtch.js
+++ sketchEtch.js
@@ -1,15 +1,11 @@
 //function to build grid in the container
 function createGrid(size) {
-
-const container = document.querySelector('#container');
-
-for (let x = 0; x < rows; x++) {
-    for (let y = 0; y < columns; y++) {
+    const container = document.querySelector('#container');
+    container.textContent = '';
+    for (let i = 0; i < size * size; i++) {
         const gridCell = document.createElement('div');
         gridCell.className = 'grid';
-        gridCell.textContent = `Cell ${x+1}-${y+1}`;
-
-        createGrid.appendChild(gridCell);
+        container.appendChild(gridCell);
     }
 }
 createGrid(16);
