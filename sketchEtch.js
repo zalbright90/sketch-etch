34443d3307599ff570d9c323b4dbc1ed25b98837
zalbright90 <61331268+zalbright90@users.noreply.github.com>
2024-06-29 04:24:55 -0700

    Set up to refactor into a function
    - Changed gridDiv to gridCell to prevent any confusion, yet remain semantic

--- sketchEtch.js
+++ sketchEtch.js
@@ -1,15 +1,15 @@
 //function to build grid in the container
-const rows = 16;
-const columns = 16;
+function createGrid(size) {
 
-const createGrid = document.querySelector('#container');
+const container = document.querySelector('#container');
 
 for (let x = 0; x < rows; x++) {
     for (let y = 0; y < columns; y++) {
-        const gridDiv = document.createElement('div');
-        gridDiv.className = 'grid';
-        gridDiv.textContent = `Cell ${x+1}-${y+1}`;
+        const gridCell = document.createElement('div');
+        gridCell.className = 'grid';
+        gridCell.textContent = `Cell ${x+1}-${y+1}`;
 
-        createGrid.appendChild(gridDiv);
+        createGrid.appendChild(gridCell);
     }
 }
+createGrid(16);
